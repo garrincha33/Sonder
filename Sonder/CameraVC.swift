60185b657310b156ce9b08fbe23bcd451e98fc0a
Richard Price <mrspondooli@gmail.com>
2017-03-08 19:15:52 +0000

    added image picker delegates via extension and tap gesture

--- Sonder/CameraVC.swift
+++ Sonder/CameraVC.swift
@@ -7,13 +7,73 @@
 //
 
 import UIKit
+import FirebaseStorage
+
 
 class CameraVC: UIViewController {
-
+    
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    var selectedImage: UIImage?
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleSelectPhoto))
+        photo.addGestureRecognizer(tap)
+        photo.isUserInteractionEnabled = true
 
- 
     }
+    
+    func handleSelectPhoto() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated:  true, completion: nil)
+        
+    }
+    
+    
+    
+    @IBAction func shareButtonPressed(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
 
 }
+
+extension CameraVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("finished picking media")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImage = image
+            photo.image = image
+        }
+        dismiss(animated: true, completion: nil)
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
