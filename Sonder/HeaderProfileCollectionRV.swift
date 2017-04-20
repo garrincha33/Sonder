2f016d9a6dc41443117fc502c5fb2eb164ffb4eb
Richard Price <mrspondooli@gmail.com>
2017-04-20 06:47:28 +0100

    added headercellVC, extended ProfileViewVC to call reuseable cell data, tested with blue background

---
+++ Sonder/HeaderProfileCollectionRV.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderProfileCollectionRV.swift
+//  Sonder
+//
+//  Created by Richard Price on 20/04/2017.
+//  Copyright © 2017 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class HeaderProfileCollectionRV: UICollectionReusableView {
+        
+}
